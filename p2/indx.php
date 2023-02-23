3937c18d19e461ebec308e93916967e4af813d4a
TottiAlfarabi <tottialfarabi@mhs.mdp.ac.id>
2023-02-23 15:06:03 +0700

    pertemuan2

--- p2/indx.php
+++ p2/indx.php
@@ -8,6 +8,51 @@ echo "Nama : $nama";
 echo "<br>Npm : $npm";
 echo "<br>hobi : $hobby[1]";
 
+echo"<ul>";
+foreach($hobby as $data){
+    echo"<li>".$data."</li>";
+}
 
+echo"</ul>";
+
+//array asodsiatif
+
+$mahasiswa = array(
+    "npm" => 2125250089,
+    "nama" => "Ahmad"
+);
+
+echo $mahasiswa['npm'];
+
+//array multidimensi
+
+$mahasiswas = [
+    [
+        "npm" => 2125250034,
+        "nama" => "Tedi"
+    ],
+    [
+         "npm" => 2125250056,
+        "nama" => "Teder"
+    ],
+    [
+         "npm" => 2125250056,
+        "nama" => "geri"
+    ]
+    ];
+
+    echo"</br></br></br></br><table border=1>
+    <tr>
+    <th>Npm</th><th>Nama Mahasiswa</th>
+    </tr>";
+foreach($mahasiswas as $data){
+    echo " <tr>
+    <td>".$data['npm']."</td>
+    <td>".$data['nama']."</td>
+    </tr>";
+}
+ echo"</table>";
+   
+   
 
 ?>
---
+++ p2/nilai.php
@@ -0,0 +1,73 @@
+<?php
+
+$nilais =  [
+    [
+        "mk" => "Algoritma Struktur Data 2",
+        "kode_mk" => "IF0017",
+        "hm" => "a",
+        "am" => 4,
+        "k" => 3,
+        "m" => 12
+        
+    ],
+     [
+        "mk" => "Aplikasi Bisnis",
+        "kode_mk" => "IF0041",
+        "hm" => "a",
+       "am" => 4,
+        "k" => 3,
+        "m" => 12
+        
+    ],
+     [
+        "mk" => "Matematika Diskrit",
+        "kode_mk" => "I1207",
+        "hm" => "a",
+       "am" => 4,
+        "k" => 4,
+        "m" => 16
+        
+    ],
+     [
+        "mk" => "Metode Numerik",
+        "kode_mk" => "IF0013",
+        "hm" => "a",
+       "am" => 4,
+        "k" => 2,
+        "m" => 8
+        
+    ],
+     [
+        "mk" => "Pemograman Berorientasi Objek",
+        "kode_mk" => "I1209",
+        "hm" => "a",
+       "am" => 4,
+        "k" => 3,
+        "m" => 12
+        
+    ],
+   
+ 
+    ];
+
+     echo"</br></br></br></br><table border=1>
+    <tr>
+        <th> Mata Kuliah </th>
+        <th>kode mk</th>
+        <th>HM</th>
+        <th>AM</th>
+        <th>K</th>
+        <th>M</th>
+    </tr>";
+foreach($nilais as $data){
+    echo " <tr>
+    <td>".$data['mk']."</td>
+    <td>".$data['kode_mk']."</td>
+   <td>".$data['hm']."</td>
+    <td>".$data['am']."</td>
+     <td>".$data['k']."</td>
+      <td>".$data['m']."</td>
+    </tr>";
+}
+ echo"</table>";
+?>
